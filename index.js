bef14acd1bb88705803aa372a05bb5cd568cddf7
hossein alijan targhi <50679946+artainjast@users.noreply.github.com>
2023-08-12 01:22:00 +0330

    feat: add getCredit to app jobs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { allRoutes } = require("./router/router");
 const app = express();
 const cron = require('node-cron');
 const {getCustomerHadTarmim} = require('./workers/remembering')
-const { sendSMS } = require("./Config/MeliPayamkconfig");
+const { getCredit} = require("./Config/MeliPayamkconfig");
 const port = process.env.NODE_ENV === 'production' ? 3000 : 3200;
 app.use(express.json());
 app.use(
@@ -19,7 +19,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).json({ message: "Internal server error" });
 })
-
+cron.schedule('0 18 * * *', async () => {
+  getCredit()
+});
 cron.schedule('0 19 * * *', async () => {
   // Main function to retrieve data
   
