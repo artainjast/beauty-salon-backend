0f1d773d76d941be78bc31c9418a2b83dcdbf2fd
hossein alijan targhi <50679946+artainjast@users.noreply.github.com>
2023-08-12 00:46:23 +0330

    feat: add some new notices

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { allRoutes } = require("./router/router");
-const app = express();
 const cron = require('node-cron');
-const {getCustomerHadTarmim} = require('./workers/remembering')
-const { sendSMS } = require("./Config/MeliPayamkconfig");
-const port = process.env.NODE_ENV === 'production' ? 3000 : 3200;
+const {sendCustomerNotice} = require('./workers/remembering')
+const app = express();
+const port = isProduction ? 3000 : 3200;
+const isProduction =  process.env.NODE_ENV === 'production' 
+
 app.use(express.json());
 app.use(
   bodyParser.urlencoded({
@@ -22,9 +23,10 @@ app.use((err, req, res, next) => {
 
 cron.schedule('0 19 * * *', async () => {
   // Main function to retrieve data
-  
-  process.env.NODE_ENV === 'production' && getCustomerHadTarmim();
-  sendSMS("09033062112" , "cron worked at 19:00 ")
+  if (isProduction) {
+    sendCustomerNotice()
+  }
+  return;
 });
 
 app.listen(port, () => {
--- workers/remembering.js
+++ workers/remembering.js
@@ -1,49 +1,125 @@
 const { Op } = require('sequelize');
 const { customerModel, receptionModel, receptionServiceModel, subServiceModel } = require('../models');
-const {rememberingCustomer} = require('../Config/MeliPayamkconfig');
+const {noticeCustomer} = require('../Config/MeliPayamkconfig');
+const { sendSMS } = require("../Config/MeliPayamkconfig");
 
-async function getCustomerHadTarmim() {
-    try {
-      // Calculate the time range
-      const threeWeeksAgo = Math.floor(Date.now() / 1000) - 21 * 24 * 60 * 60;
-      const startTime = new Date(threeWeeksAgo * 1000).setHours(19, 0, 0, 0) / 1000;
-      const endTime = new Date(startTime * 1000 + 24 * 60 * 60 * 1000).getTime() / 1000;
-      // Retrieve data using Sequelize
-      const data = await customerModel.findAll({
-        include: [
-          {
-            model: receptionModel,
-            where: {
-              created_at: {
-                [Op.between]: [startTime, endTime],
-                [Op.not] : 0
-              },
+
+const calculateTime = (day) => {
+  return Math.floor(Date.now() / 1000) - Number(day) * 24 * 60 * 60;
+}
+ 
+const getCustomerNoticeData = async () => {
+  try {
+    const customersData = await customerModel.findAll({
+      include: [
+        {
+          model: receptionModel,
+          where: {
+            created_at: {
+              [Op.between]: [time.start, time.end],
+              [Op.not] : 0
             },
-            include: [
-              {
-                model: receptionServiceModel,
-                include: [
-                  {
-                    model: subServiceModel,
-                    where: {
-                      id: [1, 2, 3 , 15] 
-                    }
+          },
+          include: [
+            {
+              model: receptionServiceModel,
+              include: [
+                {
+                  model: subServiceModel,
+                  where: {
+                    id: services
                   }
-                ]
-              }
-            ]
-          }
-        ]
-      });
-      //send sms to users
-      data.map((customer) => {
-         rememberingCustomer(customer.PHONE_NUMBER , customer.FIRST_NAME)
-      })
-    } catch (error) {
-      console.error('Error occurred:', error);
-    }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    });
+    return customersData;
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
+}
+
+const rememberNotice = async(time , services , serviceName) => {
+  try {
+    const customersData = await getCustomerNoticeData(time , services)
+    //send sms to users
+    customersData.map((customer) => {
+      noticeCustomer(customer.PHONE_NUMBER , customer.FIRST_NAME , serviceName )
+    })
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
+}
+
+//TODO : IT NEED A LOT OF REFACTOR
+const noticeTarmimPodr = async()  => {
+  const unixDay = calculateTime(27);
+  const startUnixTime = new Date(unixDay * 1000).setHours(19, 0, 0, 0) / 1000
+  rememberNotice({
+    start: startUnixTime,
+    end : new Date(startUnixTime * 1000 + 24 * 60 * 60 * 1000).getTime() / 1000
+  },
+  [48],
+  'ترمیم پودر')
+}
+
+const noticeZhelishTabie = async () => {
+  const unixDay = calculateTime(20);
+  const startUnixTime = new Date(unixDay * 1000).setHours(19, 0, 0, 0) / 1000
+  rememberNotice({
+    start: startUnixTime,
+    end : new Date(startUnixTime * 1000 + 24 * 60 * 60 * 1000).getTime() / 1000
+  },
+  [51],
+  'ژلیش ناخن طبیعی')
+}
+
+const noticeZhelish = async () => {
+  const unixDay = calculateTime(60);
+  const startUnixTime = new Date(unixDay * 1000).setHours(19, 0, 0, 0) / 1000
+  rememberNotice({
+    start: startUnixTime,
+    end : new Date(startUnixTime * 1000 + 24 * 60 * 60 * 1000).getTime() / 1000
+  },
+  [2],
+  'ژلیش')
+}
+
+const noticeTarmimPa = async () => {
+  const unixDay = calculateTime(60);
+  const startUnixTime = new Date(unixDay * 1000).setHours(19, 0, 0, 0) / 1000
+  rememberNotice({
+    start: startUnixTime,
+    end : new Date(startUnixTime * 1000 + 24 * 60 * 60 * 1000).getTime() / 1000
+  },
+  [15],
+  'ترمیم پا')
+}
+
+const noticeKashtPa = async () => {
+  const unixDay = calculateTime(60);
+  const startUnixTime = new Date(unixDay * 1000).setHours(19, 0, 0, 0) / 1000
+  rememberNotice({
+    start: startUnixTime,
+    end : new Date(startUnixTime * 1000 + 24 * 60 * 60 * 1000).getTime() / 1000
+  },
+  [15],
+  'کاشت پا')
+}
+
+
+const sendCustomerNotice =  async () => {
+  noticeTarmimPodr(); 
+  noticeZhelishTabie();
+  noticeZhelish();
+  noticeTarmimPa();
+  noticeKashtPa();
+  sendSMS("09033062112" , "cronJob worked at 19:01 ")
 }
 
 module.exports = {
-    getCustomerHadTarmim ,
+  sendCustomerNotice
 }
