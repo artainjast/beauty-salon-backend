bfd3557992c8f96d96164863933f269971d59230
hossein alijan targhi <50679946+artainjast@users.noreply.github.com>
2023-08-17 17:44:27 +0330

    fix: sendSMS problem

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { allRoutes } = require("./router/router");
 const cron = require('node-cron');
-const { getCredit} = require("./Config/MeliPayamkconfig");
+const { getCredit , sendSMS} = require("./Config/MeliPayamkconfig");
 const {sendCustomerNotice} = require('./workers/remembering')
 const app = express();
 const isProduction =  process.env.NODE_ENV === 'production' 
@@ -27,10 +27,8 @@ cron.schedule('0 18 * * *', async () => {
 cron.schedule('0 19 * * *', async () => {
   // Main function to retrieve data
   sendSMS("09033062112" , "it's reach first part ")
-
   if (isProduction) {
     sendSMS("09033062112" , `it's reach second part ${isProduction} `)
-
     sendCustomerNotice()
   }
   return;
