57464814a04337745fa21b6801b042597ed81e49
hossein alijan targhi <50679946+artainjast@users.noreply.github.com>
2023-11-10 15:31:08 +0330

    chore: active sms on production

--- utils/appFeatures/appFeaturesStatus.js
+++ utils/appFeatures/appFeaturesStatus.js
@@ -3,7 +3,7 @@ const { appFeatures } = require("../../constants/appFeatures")
 const featuresStatus = {
     [appFeatures.SENDING_REMEMBER_SMS]: {
         demo: true,
-        production : false
+        production : true
     }
 }
 
